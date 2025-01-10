d19145c4c6dad93e9f7f8fe58f4612d229d838ba
fmadera <fmadera@cerberussolutions.io>
2025-01-09 22:41:38 -0400

    admin page

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        "email",
+        "username",
+        "FullName",
+        "is_staff",
+    ]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("FullName",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("FullName",)}),)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
