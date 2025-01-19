4210cca526d85769a1fed17fd6138ae465e5e4e9
fmadera <fmadera@cerberussolutions.io>
2025-01-19 17:52:02 -0400

    el sample toma el id del usuario del jwt

--- api/samples/views.py
+++ api/samples/views.py
@@ -1,4 +1,9 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 from .serializers import SampleSerializer
 from .models import Sample
 
