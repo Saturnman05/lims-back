8af51871d2e21b607a47e6f0ad37dc3408385f4e
fmadera <fmadera@cerberussolutions.io>
2025-01-22 02:37:17 -0400

    update de samples

--- api/samples/urls.py
+++ api/samples/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import SampleCreate, SampleDetail, SampleList
+from .views import SampleCreate, SampleDetail, SampleList, sample_update
 
 urlpatterns = [
+    path("update/", sample_update),
     path("create/", SampleCreate.as_view()),
-    path("", SampleList.as_view()),
     path("<int:pk>/", SampleDetail.as_view()),
+    path("", SampleList.as_view()),
 ]
