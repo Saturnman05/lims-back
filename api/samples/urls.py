59f0529bfa54cb7b35adbb7237a03e80518e123c
the-dharex <alyomidipichardo@gmail.com>
2025-01-23 18:33:49 -0400

    Vainas de la muestra

--- api/samples/urls.py
+++ api/samples/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import SampleCreate, SampleDetail, SampleList, sample_update
+from .views import SampleCreate, SampleDetail, SampleList, sample_update, sample_assignment
 
 urlpatterns = [
     path("update/", sample_update),
     path("create/", SampleCreate.as_view()),
     path("<int:pk>/", SampleDetail.as_view()),
     path("", SampleList.as_view()),
+    path('sample-assignment/', sample_assignment),
+    path('sample-assignment/<int:sample_id>/', sample_assignment),
 ]
